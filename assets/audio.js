f2f656136bf5a2543a7a90119e9ff63b972c766b
Stuart Bell <stu-bell@users.noreply.github.com>
2025-02-03 17:34:01 +1300

    revert audio changes

--- assets/audio.js
+++ assets/audio.js
@@ -9,7 +9,6 @@ const crossfadeDuration = 4; // duration of the crossfade in seconds
 let isPlaying = false;
 const audioToggleButton = document.getElementById('audio-toggle-button');
 let buttonTimeout;
-const audioEl = document.getElementById('audio-element');
 
 // Load the audio file
 fetch('assets/audio1.m4a')
@@ -17,30 +16,8 @@ fetch('assets/audio1.m4a')
   .then(data => audioContext.decodeAudioData(data))
   .then(decodedData => {
     buffer = decodedData;
-    startMediaSession();
   });
 
-
-function startMediaSession() {
-  if ('mediaSession' in navigator) {
-    navigator.mediaSession.metadata = new MediaMetadata({
-      title: 'breathe',
-      // artist: 'Artist Name',
-      artwork: [
-        { src: 'assets/blue_sphere.png', sizes: '512x512', type: 'image/png' },
-      ]
-    });
-    navigator.mediaSession.setActionHandler('play', playAudio);
-    navigator.mediaSession.setActionHandler('pause', stopAudio);
-    navigator.mediaSession.setActionHandler('stop', stopAudio);
-  }
-}
-
-function updateMediaSessionPlaybackState() {
-  if ('mediaSession' in navigator) {
-    navigator.mediaSession.playbackState = isPlaying ? 'playing' : 'paused';
-  }
-}
 
 function playAudio() {
   console.log('playig')
@@ -48,17 +25,13 @@ function playAudio() {
   source.buffer = buffer;
   gainNode = audioContext.createGain();
   source.connect(gainNode);
-  const destination = audioContext.createMediaStreamDestination();
-  gainNode.connect(destination);
-  audioEl.srcObject = destination.stream;
+  gainNode.connect(audioContext.destination);
 
   gainNode.gain.setValueAtTime(0, audioContext.currentTime);
   gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + crossfadeDuration);
 
   source.start(0);
   isPlaying = true;
-  updateMediaSessionPlaybackState();
-  audioEl.play();
 
   // Schedule crossfade before the end of the track
   const crossfadeStartTime = buffer.duration - crossfadeDuration;
@@ -68,14 +41,12 @@ function playAudio() {
     playAudio();
   }, crossfadeStartTime * 1000);
 
-  startMediaSession();
 }
 
 function stopAudio() {
   source.stop();
   clearTimeout(crossfadeTimeout);
   isPlaying = false;
-  updateMediaSessionPlaybackState();
 }
 
 function toggleAudio() {
@@ -84,16 +55,7 @@ function toggleAudio() {
   } else {
     playAudio();
   }
-  // resetButtonTimeout();
 }
-
-// function resetButtonTimeout() {
-//   clearTimeout(buttonTimeout);
-//   audioToggleButton.style.opacity = 1;
-//   buttonTimeout = setTimeout(() => {
-//     audioToggleButton.style.opacity = 0;
-//   }, 4000);
-// }
 
 audioToggleButton.addEventListener('click', toggleAudio);
 
@@ -102,5 +64,4 @@ document.body.addEventListener('pause', () => {
   clearTimeout(crossfadeTimeout);
   isPlaying = false;
 });
-
 
